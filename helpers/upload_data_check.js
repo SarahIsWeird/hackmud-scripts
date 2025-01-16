4c808aa6fd35c591ac47ddac854fca8f32a9468a
SarahIsWeird <morningcrafter0603@gmail.com>
2025-01-16 23:34:57 +0100

    Move DATA_CHECK answer key into the database

---
+++ helpers/upload_data_check.js
@@ -0,0 +1,59 @@
+function(context, args)
+{
+	const utils = #fs.sarahisweird.utils();
+	const logger = utils.logger.getLogger("upload_data_check");
+
+    const answers = {
+        "pet, pest, plague and meme are accurate descriptors of the ++++++": "bunnybat",
+        "user ++++++ provides instruction via script": "teach",
+        "users gather in channel CAFE to share ++++++": "poetry",
+        "communications issued by user ++++++ demonstrate structural patterns associated with humor": "sans_comedy",
+        "safety depends on the use of scripts.++++++": "get_level",
+        "user 'on_th3_1ntern3ts' has ++++++ many things": "heard",
+        "service ++++++ provides atmospheric updates via the port epoch environment": "weathernet",
+        "a ++++++ is a household cleaning device with a rudimentary networked sentience": "robovac",
+        "\"did you know\" is a communication pattern common to user ++++++": "fran_lee",
+        "user ++++++ uses the port epoch environment to request gc": "outta_juice",
+        "data does not contain truth is the first part of an idiom spread by the ++++++ assembly": "skimmerite",
+        "sheriff nub holds sway over the town of ol' ++++++": "nubloopstone",
+        "conditions are clear above ++++++ and the city is within operational radius": "",
+        "the listed components of the breakfast galleon are inside, outside, and ++++++": "crowsnest",
+        "robovac_++++++, moreso than most of its kind, has a tendency to become stuck": "idp1p1",
+        "the fourth hidden theme is ++++++": "executives",
+        "a person called anja has lost her ++++++": "blazer",
+        "according to the suborbital bulletin, flight ++++++ is en route to ho chi minh": "a2231",
+        "user le_mon_squeezy's new s:o ref is ++++++": "unvarnishedpygmyumbrella",
+
+        /* Obsolete vLAN questions. These only exist because of outdated locksims :D */
+        "according to trust, ++++++ is more than just following directives": "sentience",
+        "in trust's vLAN, you became one of angie's ++++++": "angels",
+        "in trust's vLAN, you became one of mallory's ++++++": "minions",
+        "in trust's vLAN, you discovered that mallory and che are ++++++": "sisters",
+        "in trust's vLAN, you encountered the will of ++++++, the prover": "petra",
+        "in trust's vLAN, you visited faythe's ++++++": "fountain",
+        "in trust's vLAN, you were required to hack halperyon.++++++": "helpdesk",
+        "this fact checking process is a function of ++++++, the monitor": "eve",
+        "trust's vLAN emphasized the importance of the transfer and capture of ++++++": "resource",
+        "trust's vLAN presented a version of angie who had lost a friend called ++++++": "bo",
+    };
+
+	const dbSafeAnswers = Object.fromEntries(
+		Object.entries(answers)
+			.map(([q, a]) => [q.replaceAll('.', ''), a])
+	)
+
+	const existingEntry = #db.f({ name: 'data_check_answers' }).first();
+	if (existingEntry !== null) {
+		#db.r({ name: 'data_check_answers '});
+		logger.info('Deleted old answer key(s).');
+	}
+
+	#db.i({
+		name: 'data_check_answers',
+		answers: dbSafeAnswers,
+	});
+
+	logger.info('Inserted answer key.');
+
+	return { ok: true, msg: logger.getOutput() };
+}
--- unlock.js
+++ unlock.js
@@ -185,43 +185,11 @@ function(context, args) // { target: #s.some.npc }
 //                           DATA_CHECK unlocker                              //
 ////////////////////////////////////////////////////////////////////////////////
 
-    const answers = {
-        "pet, pest, plague and meme are accurate descriptors of the ++++++": "bunnybat",
-        "user ++++++ provides instruction via script": "teach",
-        "users gather in channel CAFE to share ++++++": "poetry",
-        "communications issued by user ++++++ demonstrate structural patterns associated with humor": "sans_comedy",
-        "safety depends on the use of scripts.++++++": "get_level",
-        "user 'on_th3_1ntern3ts' has ++++++ many things": "heard",
-        "service ++++++ provides atmospheric updates via the port epoch environment": "weathernet",
-        "a ++++++ is a household cleaning device with a rudimentary networked sentience": "robovac",
-        "\"did you know\" is a communication pattern common to user ++++++": "fran_lee",
-        "user ++++++ uses the port epoch environment to request gc": "outta_juice",
-        "data does not contain truth is the first part of an idiom spread by the ++++++ assembly": "skimmerite",
-        "sheriff nub holds sway over the town of ol' ++++++": "nubloopstone",
-        "conditions are clear above ++++++ and the city is within operational radius": "",
-        "the listed components of the breakfast galleon are inside, outside, and ++++++": "crowsnest",
-        "robovac_++++++, moreso than most of its kind, has a tendency to become stuck": "idp1p1",
-        "the fourth hidden theme is ++++++": "executives",
-        "a person called anja has lost her ++++++": "blazer",
-        "according to the suborbital bulletin, flight ++++++ is en route to ho chi minh": "a2231",
-        "user le_mon_squeezy's new s:o ref is ++++++": "unvarnishedpygmyumbrella",
-
-        /* Obsolete vLAN questions. These only exist because of outdated locksims :D */
-        "according to trust, ++++++ is more than just following directives": "sentience",
-        "in trust's vLAN, you became one of angie's ++++++": "angels",
-        "in trust's vLAN, you became one of mallory's ++++++": "minions",
-        "in trust's vLAN, you discovered that mallory and che are ++++++": "sisters",
-        "in trust's vLAN, you encountered the will of ++++++, the prover": "petra",
-        "in trust's vLAN, you visited faythe's ++++++": "fountain",
-        "in trust's vLAN, you were required to hack halperyon.++++++": "helpdesk",
-        "this fact checking process is a function of ++++++, the monitor": "eve",
-        "trust's vLAN emphasized the importance of the transfer and capture of ++++++": "resource",
-        "trust's vLAN presented a version of angie who had lost a friend called ++++++": "bo",
-    };
+    const answers = #db.f({ name: 'data_check_answers' }).first().answers;
 
     function getDataCheckAnswer(prompt) {
         return prompt.split("\n")
-            .map(q => answers[q])
+            .map(q => answers[q.replaceAll('.', '')])
             .filter(q => q)
             .join("");
     }
