e9c861b2410aa93a4902142acb6291a89195ffda
SarahIsWeird <morningcrafter0603@gmail.com>
2025-02-04 20:40:29 +0100

    Fix uploading for tier scripts

---
+++ src/lib/hecking/tiers/tier1.ts
@@ -0,0 +1,31 @@
+import { EzDigitSolver, EzPrimeSolver, EzSolver } from '/lib/hecking/solvers/ez';
+import { C00xSolver } from '/lib/hecking/solvers/c00x';
+import { L0cketSolver } from '/lib/hecking/solvers/l0cket';
+import { DataCheckSolver } from '/lib/hecking/solvers/data_check';
+import { Solver, SolverConstructor } from '/lib/hecking/common';
+import { SolverTier } from '/lib/hecking/solvers';
+
+const t1SolverClasses = {
+    ezSolver: EzSolver,
+    ezDigitSolver: EzDigitSolver,
+    ezPrimeSolver: EzPrimeSolver,
+    c00xSolver: C00xSolver,
+    l0cketSolver: L0cketSolver,
+    dataCheckSolver: DataCheckSolver,
+} satisfies Record<string, SolverConstructor<any>>;
+
+export type T1SolverNames = keyof typeof t1SolverClasses;
+export type T1Solvers = Record<T1SolverNames, Solver<any>>;
+
+export class T1SolverTier implements SolverTier<T1Solvers> {
+    getName(): string {
+        return 'T1SolverTier';
+    }
+
+    getSolvers() {
+        return t1SolverClasses;
+    }
+
+    onStateLoad(): void {}
+    onStateSave(): void {}
+}
--- src/scripts/sarahisweird/t1_solvers.ts
+++ src/scripts/sarahisweird/t1_solvers.ts
@@ -1,34 +1,4 @@
-import { EzDigitSolver, EzPrimeSolver, EzSolver } from '/lib/hecking/solvers/ez';
-import { C00xSolver } from '/lib/hecking/solvers/c00x';
-import { L0cketSolver } from '/lib/hecking/solvers/l0cket';
-import { SolverTier } from '/lib/hecking/solvers';
-import { Solver, SolverConstructor } from '/lib/hecking/common';
-import { DataCheckSolver } from '/lib/hecking/solvers/data_check';
-
-const t1SolverClasses = {
-    ezSolver: EzSolver,
-    ezDigitSolver: EzDigitSolver,
-    ezPrimeSolver: EzPrimeSolver,
-    c00xSolver: C00xSolver,
-    l0cketSolver: L0cketSolver,
-    dataCheckSolver: DataCheckSolver,
-} satisfies Record<string, SolverConstructor<any>>;
-
-export type T1SolverNames = keyof typeof t1SolverClasses;
-export type T1Solvers = Record<T1SolverNames, Solver<any>>;
-
-export class T1SolverTier implements SolverTier<T1Solvers> {
-    getName(): string {
-        return 'T1SolverTier';
-    }
-
-    getSolvers() {
-        return t1SolverClasses;
-    }
-
-    onStateLoad(): void {}
-    onStateSave(): void {}
-}
+import { T1SolverTier } from '/lib/hecking/tiers/tier1';
 
 export default function (_context: Context, _args?: unknown) {
     return new T1SolverTier();
